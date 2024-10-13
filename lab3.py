9f423fd8bd3335f2f982c1503b766a941e37d046
Анастасия Кузьмина <kuzmina_anst@mail.ru>
2024-10-13 19:15:25 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -42,3 +42,37 @@ def form1():
 
     sex = request.args.get('sex')
     return render_template('/lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order/')
+def order():
+    return render_template('lab3/order.html')
+
+
+price = 0
+
+@lab3.route('/lab3/pay/')
+def pay():
+    global price
+
+    drink = request.args.get('drink')
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+        
+    return render_template('lab3/pay.html', price=price)
+
+
+@lab3.route('/lab3/paid/')
+def paid():
+    global price 
+    
+    return render_template('lab3/paid.html', price=price)
