43ae2de33e7a6e6425ecaf4fb835234a225d7171
Анастасия Кузьмина <kuzmina_anst@mail.ru>
2024-12-18 23:15:24 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,39 +1,38 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import sqlite3
-from os import path
-from flask import  render_template, Blueprint, request, jsonify, current_app
+from flask import  render_template, Blueprint, request, redirect 
 from datetime import datetime
+from db import db
+from db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint('lab8', __name__)
 
 
-def db_connect():
-    if current_app.config['DB_TYPE'] == 'postgres':
-        conn = psycopg2.connect(
-            host = '127.0.0.1',
-            database = 'anastasia_kuzmina_knowledge_base',
-            user = 'anastasia_kuzmina_knowledge_base',
-            password = '123'
-        )
-        cur = conn.cursor(cursor_factory = RealDictCursor)
-    else:
-        dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
-        conn = sqlite3.connect(db_path)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-
-    return conn, cur
-
-def db_close(conn, cur):
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
 @lab8.route('/lab8/')
 def main():
-    return render_template('lab7/lab7.html')
+    return render_template('lab8/lab8.html')
 
+
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/register.html', error = 'Заполните все поля!')
+
+    if not password_form:
+        return render_template('lab8/register.html', error = 'Заполните все поля!')
+
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error = 'Такой пользователь уже существует')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
 
